17b9caa4ef221b6b41c611c8328ec580142f4ff2
Giorgio Gamba <giorgiogamba97@gmail.com>
2025-06-06 18:05:06 +0200

    add terminal raw mode

--- src/minion.cpp
+++ src/minion.cpp
@@ -3,9 +3,27 @@
 // Minion, a c++ text editor
 
 #include <iostream>
+#include <termios.h>
+#include <unistd.h>
+
+void enableTerminalRawMode()
+{
+	// Read terminal configurations
+	struct termios raw;
+	tcgetattr(STDIN_FILENO, &raw);
+
+	// Turn off echo mode (each key is printed on terminal)
+	raw.c_lflag &= ~(ECHO);
+
+	// Overwrite terminal configurations
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
 
 int main()
 {
+	// Change terminal configurations
+	enableTerminalRawMode();
+
 	std::cout << "Test\n";
 	return 0;
 }
